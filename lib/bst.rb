1630ba0d51c201b2f729ada6bf18f73f3eda89ef
Robbie Lane <lane.robbie@gmail.com>
2015-09-06 18:19:44 -0600

    fixed include? and added sort

--- lib/bst.rb
+++ lib/bst.rb
@@ -22,23 +22,6 @@ class Bst
     end
   end
 
-  def include?(data)
-    return true if data == @head.data
-    cursor = find_traverse(data)
-    data == cursor.data ? true : false
-  end
-
-  def depth_of(data)
-    depth = 0
-    return nil if !head
-    if data == @head.data
-      depth += 1
-    else
-      #do this
-    end
-    depth
-  end
-
   def traverse(data, cursor=@head)
     return cursor if cursor.data == data
     if data <= cursor.data && !cursor.left.nil?
@@ -51,14 +34,89 @@ class Bst
     return cursor
   end
 
+  def minimum(cursor=@head)
+    return if !head
+    until cursor.left.nil? do
+      cursor = cursor.left
+    end
+    cursor.data
+  end
+
+  def maximum(cursor=@head)
+    return if !head
+    until cursor.right.nil? do
+      cursor = cursor.right
+    end
+    cursor.data
+  end
+
+  def include?(data)
+    return true if data == @head.data
+    find_traverse(data)
+    data == @cursordata ? true : false
+  end
+
   def find_traverse(data, cursor=@head)
+    find_traverse(data, cursor.left) if !cursor.left.nil?
+    @cursordata = cursor.data if data == cursor.data
+    #return cursor.data if cursor.data == data
+    find_traverse(data, cursor.right) if !cursor.right.nil?
+  end
+
+  def depth_of(data)
+    depth = 0
+    return nil if !head
+    if data == @head.data
+      depth += 1
+    else
+      depth = find_depth_traverse(data)
+    end
+    depth
+  end
+
+  def find_depth_traverse(data, depth=1, cursor=@head)
     if data < cursor.data && !cursor.left.nil?
       cursor = cursor.left
-      cursor = traverse(data, cursor) if data < cursor.data && !cursor.left.nil?
+      depth += 1
+      cursor = find_depth_traverse(data, depth, cursor) if cursor.data != data
     elsif data > cursor.data && !cursor.right.nil?
       cursor = cursor.right
-      cursor = traverse(data, cursor) if data > cursor.data && !cursor.right.nil?
-    end
-    return cursor
+      depth += 1
+      cursor = find_depth_traverse(data, depth, cursor) if cursor.data != data
+      end
+    depth
   end
+
+  def sort
+    sorted = []
+    sort_traverse(sorted)
+    sorted
+  end
+
+  def sort_traverse(sorted, cursor=@head)
+    return if !cursor
+    sort_traverse(sorted, cursor.left)
+    sorted << cursor.data
+    sort_traverse(sorted, cursor.right)
+    sorted
+  end
+
+  # def find_traverse(data, cursor=@head)
+  #   if data < cursor.data && !cursor.left.nil?
+  #     cursor = cursor.left
+  #     cursor = traverse(data, cursor) if data < cursor.data && !cursor.left.nil?
+  #   elsif data > cursor.data && !cursor.right.nil?
+  #     cursor = cursor.right
+  #     cursor = traverse(data, cursor) if data > cursor.data && !cursor.right.nil?
+  #   end
+  #   return cursor
+  # end
 end
+
+# tree = Bst.new
+# File.open('input.txt').readlines.each do |line|
+#   tree.insert(line.chomp.to_i)
+# end
+# p tree
+#
+# tree.traverse_all
--- test/bst_test.rb
+++ test/bst_test.rb
@@ -21,7 +21,7 @@ class BstTest < Minitest::Test
     tree.insert(1)
 
     assert_equal 1, tree.head.left.data
- end
+  end
 
   def test_equal_data_goes_left
     tree = Bst.new
@@ -48,70 +48,111 @@ class BstTest < Minitest::Test
     tree.insert(1)
 
     assert_equal 1, tree.head.left.left.left.left.data
- end
+  end
 
- def test_greater_than_continues_to_go_right
-   tree = Bst.new
-   tree.insert(1)
-   tree.insert(2)
-   tree.insert(3)
-   tree.insert(4)
-   tree.insert(5)
+  def test_greater_than_continues_to_go_right
+    tree = Bst.new
+    tree.insert(1)
+    tree.insert(2)
+    tree.insert(3)
+    tree.insert(4)
+    tree.insert(5)
 
-   assert_equal 5, tree.head.right.right.right.right.data
- end
+    assert_equal 5, tree.head.right.right.right.right.data
+  end
 
- def test_include_verifys_a_value
-   tree = Bst.new
-   tree.insert(6)
-   tree.insert(5)
-   tree.insert(7)
-   tree.insert(4)
-   tree.insert(8)
-   tree.insert(3)
-   tree.insert(9)
+  def test_include_verifys_a_value
+    tree = Bst.new
+    tree.insert(6)
+    tree.insert(3)
+    tree.insert(9)
+    tree.insert(7)
+    tree.insert(8)
+    tree.insert(5)
+    tree.insert(4)
 
-   assert tree.include?(8)
- end
+    assert tree.include?(8)
+  end
 
- def test_include_rejects_a_value
-   tree = Bst.new
-   tree.insert(6)
-   tree.insert(3)
-   tree.insert(9)
-   tree.insert(7)
-   tree.insert(8)
-   tree.insert(5)
-   tree.insert(4)
+  def test_include_rejects_a_value
+    tree = Bst.new
+    tree.insert(6)
+    tree.insert(3)
+    tree.insert(9)
+    tree.insert(7)
+    tree.insert(8)
+    tree.insert(5)
+    tree.insert(4)
 
-   refute tree.include?(10)
- end
+    refute tree.include?(10)
+  end
 
- def test_depth_of_finds_depth_of_head
-   tree = Bst.new
-   tree.insert(6)
-   tree.insert(5)
-   tree.insert(7)
+  def test_depth_of_finds_depth_of_head
+    tree = Bst.new
+    tree.insert(6)
+    tree.insert(5)
+    tree.insert(7)
 
-   assert_equal 1, tree.depth_of(6)
- end
+    assert_equal 1, tree.depth_of(6)
+  end
 
- def test_depth_of_returns_nil_if_empty
-   tree = Bst.new
+  def test_depth_of_returns_nil_if_empty
+    tree = Bst.new
 
-   assert_nil tree.depth_of(1)
- end
+    assert_nil tree.depth_of(1)
+  end
 
- def test_depth_of_continues_to_return_depth
-   tree = Bst.new
-   tree.insert(6)
-   tree.insert(3)
-   tree.insert(9)
-   tree.insert(7)
-   tree.insert(8)
-   tree.insert(5)
-   tree.insert(4)
+  def test_depth_of_continues_to_return_depth
+    tree = Bst.new
+    tree.insert(6)
+    tree.insert(3)
+    tree.insert(9)
+    tree.insert(7)
+    tree.insert(8)
+    tree.insert(5)
+    tree.insert(4)
 
-   assert tree.depth_of(1)
- end
+    assert_equal 3, tree.depth_of(5)
+  end
+
+  def test_maximum_finds_highest_value
+    tree = Bst.new
+    tree.insert(8)
+    tree.insert(4)
+    tree.insert(12)
+    tree.insert(2)
+    tree.insert(3)
+    tree.insert(9)
+    tree.insert(14)
+
+    assert_equal 14, tree.maximum
+  end
+
+  def test_minimum_finds_lowest_value
+    tree = Bst.new
+    tree.insert(8)
+    tree.insert(4)
+    tree.insert(12)
+    tree.insert(2)
+    tree.insert(3)
+    tree.insert(9)
+    tree.insert(14)
+
+    assert_equal 2, tree.minimum
+  end
+
+  def test_sort_with_8_elements
+    tree = Bst.new
+    tree.insert(8)
+    tree.insert(4)
+    tree.insert(12)
+    tree.insert(2)
+    tree.insert(3)
+    tree.insert(9)
+    tree.insert(14)
+    tree.insert(1)
+
+    assert_equal [1, 2, 3, 4, 8, 9, 12, 14], tree.sort
+  end
+
 end
